4579c77ca72bd244b6158825468700e8becb7158
Shuranova N <ni.shuranova@gmail.com>
2023-01-10 17:13:08 +0300

    Correction

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,4 @@
 public class BmiService {
-    double bodyWeigt;
-    double height;
     public double calculate (double bodyWeight, double height){
         height = height / 100;
         double bmi = bodyWeight / (Math.pow(height , 2));
